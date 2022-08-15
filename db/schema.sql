8d0c4341999c91f938b971e86ec85defcef89c34
tonylf <tonylonefight@gmail.com>
2022-08-15 05:27:52 -0500

    Eureka! Finally got the tables to work and show up.

--- db/schema.sql
+++ db/schema.sql
@@ -3,20 +3,52 @@ CREATE DATABASE firefly;
 
 USE firefly;
 
-CREATE TABLE firefly.department (
+CREATE TABLE department (
     id INT PRIMARY KEY, 
-    name VARCHAR(30)
+    depname VARCHAR(30)
 );
-CREATE TABLE firefly.role (
+CREATE TABLE crewrole (
     id INT PRIMARY KEY, 
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT 
 );
-CREATE TABLE firefly.employee (
+CREATE TABLE employee (
     id INT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT
 );
+
+INSERT INTO department (id, depname) 
+    VALUES 
+        (201, 'Command'),
+        (202, 'Medical'),
+        (203, 'Maintenance'),
+        (204, 'Civilian');
+
+INSERT INTO crewrole (id, title, salary, department_id)  
+	VALUES 
+        (301, 'Captain', 12000.00, 201),
+	    (302, 'First Lieutenant', 12000.00, 201),
+	    (303, 'Pilot', 10000.00, 201),	
+	    (305, 'Soldier', 8000.00, 201),
+	    (304, 'Doctor', 10000.00, 202),
+	    (306, 'Chief Engineer', 12000.00, 203),
+	    (307, 'Passenger', 6000.00, 204),
+	    (308, 'Psychiatrists', 10000.00, 202);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+	VALUES  
+        (101, 'Malcome', 'Reynolds', 301, null),
+	    (102, 'Zoe', 'Washburne', 302, 101),
+	    (103, 'Hoban', 'Washburne', 303, 102),
+	    (104, 'Simon', 'Tam', 304, 101),
+	    (105, 'Jayne', 'Cobb', 305, 102),
+	    (106, 'Kaylee', 'Frye', 306, 101),
+	    (107, 'Inara', 'Serra', 307, null),
+	    (108, 'River', 'Tam', 307, 104),
+	    (109, 'Shepherd', 'Book', 307, 104);
+
+SELECT * FROM crewrole;
--- db/seed.sql
+++ db/seed.sql
@@ -1,26 +1,29 @@
-INSERT INTO department ('id', 'name') 
-    VALUES ('201', 'Command'),
-    VALUES ('202', 'Medical'),
-    VALUES ('203', 'Maintenance'),
-    VALUES ('204', 'Civilian');
+INSERT INTO department (id, depname) 
+    VALUES 
+        (201, 'Command'),
+        (202, 'Medical'),
+        (203, 'Maintenance'),
+        (204, 'Civilian');
 
-INSERT INTO role ('id', 'title', 'salary', 'department_id')  
-	VALUES ('301', 'Captain', '12000.00', '201'),
-	VALUES ('302', 'First Lieutenant', '12000.00', '201'),
-	VALUES ('303', 'Pilot', '10000.00', '201'),	
-	VALUES ('305', 'Soldier', '8000.00', '201'),
-	VALUES ('304', 'Doctor', '10000.00', '202'),
-	VALUES ('306', 'Chief Engineer', '12000.00', '203'),
-	VALUES ('307', 'Passenger', '6000.00', '204')
-	VALUES ('308', 'Psychiatrists', '10000.00', '202');
+INSERT INTO crewrole (id, title, salary, department_id)  
+	VALUES 
+        (301, 'Captain', 12000.00, 201),
+	    (302, 'First Lieutenant', 12000.00, 201),
+	    (303, 'Pilot', 10000.00, 201),	
+	    (305, 'Soldier', 8000.00, 201),
+	    (304, 'Doctor', 10000.00, 202),
+	    (306, 'Chief Engineer', 12000.00, 203),
+	    (307, 'Passenger', 6000.00, 204),
+	    (308, 'Psychiatrists', 10000.00, 202);
 
-INSERT INTO employee ('id', 'first_name', 'last_name', 'role_id', 'manager_id')
-	VALUES ('101', 'Malcome', 'Reynolds', '301', 'null'),
-	VALUES ('102', 'Zoe', 'Washburne', '302', '101'),
-	VALUES ('103', 'Hoban', 'Washburne', '303', '102'),
-	VALUES ('104', 'Simon', 'Tam', '304', '101'),
-	VALUES ('105', 'Jayne', 'Cobb', '305', '102'),
-	VALUES ('106', 'Kaylee', 'Frye', '306', '101'),
-	VALUES ('107', 'Inara', 'Serra', '307', 'null'),
-	VALUES ('108', 'River', 'Tam', '307', '104'),
-	VALUES ('109', 'Shepherd', 'Book', '307', '104');
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+	VALUES  
+        (101, 'Malcome', 'Reynolds', 301, null),
+	    (102, 'Zoe', 'Washburne', 302, 101),
+	    (103, 'Hoban', 'Washburne', 303, 102),
+	    (104, 'Simon', 'Tam', 304, 101),
+	    (105, 'Jayne', 'Cobb', 305, 102),
+	    (106, 'Kaylee', 'Frye', 306, 101),
+	    (107, 'Inara', 'Serra', 307, null),
+	    (108, 'River', 'Tam', 307, 104),
+	    (109, 'Shepherd', 'Book', 307, 104);
