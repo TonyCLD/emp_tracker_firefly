525fe254e052b076a1171064b81986d655568d90
tonylf <tonylonefight@gmail.com>
2022-08-17 00:32:22 -0500

    trying to re-start project, not getting much traction

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,16 +1,13 @@
 const emp_router = require('express').Router();
-const fs = require('fs');
+const db = require('../db/connection');
+// const fs = require('fs');
 const path = require ('path');
-const uuid = require ('uuid').v4;
-const mysql = require('mysql2');
+const connection = require('../db/connection');
+// const uuid = require ('uuid').v4;
+// const mysql = require('mysql2');
 const db_path = path.join(__dirname, '../db/emp.json');
 
-const connection = mysql.createPool({
-    host: 'localhost',
-    database: 'firefly',
-    user: 'root',
-    password: 'pass'
-});
+
 
 connection.query('SELECT * FROM employee', (err, data) => {
     if (err) return console.log(err);
