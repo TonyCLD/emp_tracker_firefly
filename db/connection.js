25083a19f0ed9e70c1afccda5ee0c56def02ae94
tonylf <tonylonefight@gmail.com>
2022-08-17 22:43:10 -0500

    adding jaws connection

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createPool({
-    host: 'localhost',
-    database: 'firefly',
-    user: 'root',
-    password: 'pass'
+    host: 'n4m3x5ti89xl6czh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    database: 'owpggkyq543hb11k',
+    user: 'nuk599y5hqmsbyqw',
+    password: 'wucrrz55j4nzhdap'
 });
+
+// PORT 3306
 
 module.exports = connection;
